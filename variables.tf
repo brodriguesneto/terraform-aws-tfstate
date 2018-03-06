18343d9ee9c91552059f9c16ba77a4f55cb68f1f
Boaventura Rodrigues Neto <brodriguesneto@gmail.com>
2018-03-06 00:07:28 -0300

    Added default values

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "s3_bucket_name" {
   description = "Name of the S3 Bucket to server as backend"
-  default     = ""
+  default     = "tfstatetestename0001"
 }
 
 variable "dynamodb_table_name" {
   description = "Name of the DynamoDB Table to use state lock feature"
-  default     = ""
+  default     = "tfstatetestename0001"
 }
